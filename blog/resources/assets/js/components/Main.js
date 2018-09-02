fc40f5a0029da53726a1eaa3e3701f195c00bc1e
Vachet Virginie <vachetvirginie@gmail.com>
2018-09-02 14:18:42 +0200

    test

--- blog/resources/assets/js/components/Main.js
+++ blog/resources/assets/js/components/Main.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Product from './Product';
-import AddProduct from './AddProduct';
-
+ 
 /* Main Component */
 class Main extends Component {
-
+ 
   constructor() {
-  
+   
     super();
     //Initialize the state in the constructor
     this.state = {
         products: [],
-        currentProduct: null
-    
     }
-     this.handleAddProduct = this.handleAddProduct.bind(this);
   }
   /*componentDidMount() is a lifecycle method
    * that gets called after the component is rendered
@@ -31,104 +26,29 @@ class Main extends Component {
             this.setState({ products });
         });
   }
-
+ 
  renderProducts() {
-        const listStyle = {
-            listStyle: 'none',
-            fontSize: '18px',
-            lineHeight: '1.8em',
-        }
     return this.state.products.map(product => {
         return (
             /* When using list you need to specify a key
              * attribute that is unique for each list item
             */
-            <li style={listStyle} onClick={
-                () =>this.handleClick(product)} key={product.id} >
+            <li key={product.id} >
                 { product.title } 
             </li>      
         );
     })
   }
-
-  handleClick(product) {
-
-      //handleClick is used to set the state
-      this.setState({currentProduct:product});
-  
-  }
-
-   handleAddProduct(product) {
-     
-    product.price = Number(product.price);
-    /*Fetch API for post request */
-    fetch( 'api/products/', {
-        method:'post',
-        /* headers are important*/
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        
-        body: JSON.stringify(product)
-    })
-    .then(response => {
-        return response.json();
-    })
-    .then( data => {
-       
-        this.setState((prevState)=> ({
-            products: prevState.products.concat(data),
-            currentProduct : data
-        }))
-    })
- //update the state of products and currentProduct
-  }  
-    
+   
   render() {
-
-   const mainDivStyle =  {
-        display: "flex",
-        flexDirection: "row"
-    }
-    
-    const divStyle = {
-       
-        justifyContent: "flex-start",
-        padding: '10px',
-        width: '35%',
-        background: '#f0f0f0',
-        padding: '20px 20px 20px 20px',
-        margin: '30px 10px 10px 30px'
-        
-    }
-
+   /* Some css code has been removed for brevity */
     return (
         <div>
-          <div style= {mainDivStyle}>
-            <div style={divStyle}>
-                <h3> All products </h3>
-                  <ul>
-                    { this.renderProducts() }
-                  </ul> 
-
+              <ul>
+                { this.renderProducts() }
+              </ul> 
             </div> 
-                <Product product={this.state.currentProduct} />
-                <AddProduct onAdd={this.handleAddProduct} /> 
-          </div>
-              
-        </div>
-      
+       
     );
   }
-}
-
-export default Main;
-
-/* The if statement is required so as to Render the component 
- * on pages that have a div with an ID of "root";  
- */ 
-
-if (document.getElementById('root')) {
-    ReactDOM.render(<Main />, document.getElementById('root'));
 }
--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -2,15 +2,16 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel React application</title>
-        <meta name="csrf-token" content="{{ csrf_token() }}"></meta>
+        <title>Product listing Application</title>
         <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
-    <h2 style="text-align: center"> Laravel and React application </h2>
+        <h1 style="text-align: center"> Laravel and React application </h1>
         <div id="root"></div>
+       
         <script src="{{mix('js/app.js')}}" ></script>
     </body>
 </html>
